944446ad4fd73210974aca0125cac8f05b193707
aria <aryashahbazi@gmail.com>
2021-03-23 00:17:21 +0430

    ASCII

---
+++ Build/tests/CMakeFiles/unit_tests.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../unit_tests.exe"
+  "../unit_tests.exe.manifest"
+  "../unit_tests.pdb"
+  "CMakeFiles/unit_tests.dir/foo.cpp.obj"
+  "CMakeFiles/unit_tests.dir/foo.cpp.obj.d"
+  "CMakeFiles/unit_tests.dir/main.cpp.obj"
+  "CMakeFiles/unit_tests.dir/main.cpp.obj.d"
+  "libunit_tests.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unit_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: G:/C++/AW/ASCII/tests
+# Build directory: G:/C++/AW/ASCII/Build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(myLibrary.unit_tests "G:/C++/AW/ASCII/Build/unit_tests.exe")
+set_tests_properties(myLibrary.unit_tests PROPERTIES  _BACKTRACE_TRIPLES "G:/C++/AW/ASCII/tests/CMakeLists.txt;22;add_test;G:/C++/AW/ASCII/tests/CMakeLists.txt;0;")
